1a3c97f037d672487bae7a17af4435fcfef24558
demyan1805 <demyan1805@gmail.com>
2019-06-12 16:21:05 +0500

    fixed bg

--- js/script.js
+++ js/script.js
@@ -7,8 +7,6 @@ window.addEventListener('DOMContentLoaded', function() {
         next = document.querySelectorAll('.slider__btn')[1];
     showSlides(slideIndex);
     function showSlides(n) {
-        
-        console.log('before'+slideIndex);
         if (slideIndex[1] > sliderItems.length -1) {
             slideIndex[1] = 0;
             sliderItems[sliderItems.length -1].style.order = -1;
@@ -29,15 +27,9 @@ window.addEventListener('DOMContentLoaded', function() {
         if (slideIndex[1] < 0) {
             slideIndex[1] = sliderItems.length -1;
         }
-        // else {
-        //     slideIndex = n;
-        // }
-        console.log('after'+slideIndex);
         sliderItems.forEach((item) => item.classList.remove('active'));
         sliderItems[slideIndex[0]].classList.add('active');
         sliderItems[slideIndex[1]].classList.add('active');
-
-        
     }
     prev.addEventListener('click', function() {
         showSlides([--slideIndex[0], --slideIndex[1]]);
